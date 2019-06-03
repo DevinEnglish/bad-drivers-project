c0a83ed7ca2decff89dbe8cd6ab32d380afc1a46
Devin English <Devine4@UW.edu>
2019-06-03 12:15:27 -0700

    added extra graphs to table tabs

--- server.R
+++ server.R
@@ -239,28 +239,139 @@ shinyServer(function(input, output) {
                                   "Percentage of drivers who were alcohol impaired"=colnames(bad_driving)[4],
                                   "Percentage of drivers who were NOT distracted"=colnames(bad_driving)[5],
                                   "Percentage of drivers who had not been involved in previous accidents"=colnames(bad_driving)[6],
-                                  "Number of car insurance premiums"=colnames(bad_driving)[7],
-                                  "Number of losses incurred by insurance companies per insured driver"=colnames(bad_driving)[8])
+                                  "Price of car insurance premiums"=colnames(bad_driving)[7],
+                                  "Cost of losses incurred by insurance companies per insured driver"=colnames(bad_driving)[8])
   })
   output$stateAccidentsTable <- renderTable({
     state_accidents %>% dplyr::rename("Other/Unkown"=colnames(state_accidents)[7])
   })
   output$alcoholLevelsTable <- renderTable({
-    alcohol_levels %>% dplyr::rename("BAC = 0"=BAC_0,"0.01<BAC>0.07"=BAC_0.01_0.07,
-                                     "BAC>0.08"=BAC_over_0.08,"BAC>0.01"=BAC_over_0.01)
+    alcohol_levels %>% dplyr::rename("BAC = 0"=`BAC_0.00`,"0.01<BAC>0.07"=`BAC_0.01_0.07`,
+                                     "BAC>0.08"=`BAC_OVER_0.08`,"BAC>0.01"= `BAC_over_0.01`)
+  })
+  Alcohol_content_plot <- reactive({
+    alcohol_levels <- alcohol_levels %>% select(-c(BAC_over_0.01, Total))
+    alcohol_levels <- gather(alcohol_levels, level, amount, 2:4)
+    alcohol_levels <- alcohol_levels %>% mutate(level = gsub("X","",level)) %>% mutate(level = gsub("(.)_(.)", "\\1 - \\2",level)) %>%
+    filter(year == input$yearForAlcGraph)
+    alcohol_bargraph <-ggplot(data= alcohol_levels, aes(x= level, y=amount)) +
+      expand_limits(y=c(0,30000))+
+      geom_bar(stat="identity", fill="tan1") +
+      theme_economist()+
+      labs(title = paste("Alcohol content of drivers involved in accidents in", input$yearForAlcGraph), y = "Number of accidents", x = "Alcohol level",
+           caption = "BAC = Blood Alcohol Content\n Legally impaired with BAC of 0.08 + in the United States") # date changes with input
+    print(alcohol_bargraph)
+  })
+  output$alcoholLevelsGraph <- renderPlot({
+    Alcohol_content_plot()
   })
   output$monthTable <- renderTable({
     month %>% dplyr::rename("Property Damage"=colnames(month)[4])
+  })
+  by_month_graph <- reactive ({
+    if(input$byMonthAccidentType == "Fatal"){
+      accidentType <- month$Fatal
+    }
+    if(input$byMonthAccidentType == "Injury Only"){
+      accidentType <- month$Injury
+    }
+    if(input$byMonthAccidentType == "Property Damage"){
+      accidentType <- month$property.damage
+    }
+    month_order <- factor(month$Month, level = c('Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept',
+                                                 'Oct', 'Nov', 'Dec'))
+    month_bargraph <-ggplot(data= month, aes(x= month_order, y= accidentType)) + 
+      geom_bar(stat="identity", fill="tan1") +
+      theme_economist()+
+      labs(title = paste("Accidents (",input$byMonthAccidentType, ") by Month in 2017", sep=""), x = "Month", y = "Number of accidents") + # accident type changes
+      theme(plot.title = element_text(hjust = 0.5))
+    print(month_bargraph)
+  })
+  output$byMonthGraph <- renderPlot({
+    by_month_graph()
   })
   output$severityTable <- renderTable({
     severity %>% dplyr::rename("Percent fatal (%)"=percent.fatal,"Fatalities"=fatal,"Percent injuries (%)"=percent.injury,
                                "Percent property damage (%)"=percent.property.damage)
   })
+  severity_graph <- reactive({
+    if(input$severityAccidentType == "Fatal"){
+      accidentType <- severity$fatal
+    }
+    if(input$severityAccidentType == "Injury Only"){
+      accidentType <- severity$injury
+    }
+    if(input$severityAccidentType == "Property Damage"){
+      accidentType <- severity$property.damage
+    }
+    severity_line <- ggplot(data= severity, aes(x=year, y= accidentType, group=1)) +
+      geom_line(color="steelblue4", size = 1.5)+
+      geom_point(color = "tan1", size = 3.5) +
+      theme_economist()+
+      labs(title = paste("Number of", input$severityAccidentType,"accidents from 1988-2017"), x = "Year", y = "Number of accidents") + # accident type changes
+      theme(plot.title = element_text(hjust = 0.5))
+    print(severity_line)
+  })
+  output$severityGraph <- renderPlot({
+    severity_graph()
+  })
   output$timeOfDayTable <- renderTable({
     time_of_day %>% dplyr::rename("Time"=TOD)
   })
+  TOD_graph <- reactive({
+    if(input$dayOfWeek == "") {
+      day <- time_of_day$Monday
+    }
+    if(input$dayOfWeek == "Monday") {
+      day <- time_of_day$Monday
+    }
+    if(input$dayOfWeek == "Tuesday") {
+      day <- time_of_day$Tuesday
+    }
+    if(input$dayOfWeek == "Wednesday") {
+      day <- time_of_day$Wednesday
+    }
+    if(input$dayOfWeek == "Thursday") {
+      day <- time_of_day$Thursday
+    }
+    if(input$dayOfWeek == "Friday") {
+      day <- time_of_day$Friday
+    }
+    if(input$dayOfWeek == "Saturday") {
+      day <- time_of_day$Saturday
+    }
+    if(input$dayOfWeek == "Sunday") {
+      day <- time_of_day$Sunday
+    }
+    time_of_day <- time_of_day[-(9:27),]
+    TOD_order <- factor(time_of_day$TOD, level = c('12-3AM','3-6AM','6-9AM','9AM-12PM','12-3PM','3-6PM',"6-9PM", '9PM-12AM')) 
+    TOD_line <- ggplot(data= time_of_day, aes(x= TOD_order, y= day, group=1)) +
+      geom_line(color="steelblue4", size = 1.5)+
+      theme_economist()+
+      geom_point(color = "tan1", size = 3.5) +
+      labs(title = paste("Average Number of Accidents on", day, "in 2017"), x = "Time of Day", y = "Number of accidents") + # accident type changes
+      theme(plot.title = element_text(hjust = 0.5))
+    print(TOD_line)
+  })
+  output$TODGraph <- renderPlot({
+    TOD_graph()
+  })
   output$driverDemographicTable <- renderTable({
     driver_demographic
+  })
+  demo_graph <- reactive({
+    driver_demographic <- driver_demographic %>% filter(Age == input$age) # change in age
+    driver_demographic <- gather(driver_demographic, sex, amount, 2:3)
+    age_bargraph <-ggplot(data= driver_demographic, aes(x= sex, y= amount)) + # y changes with input
+      geom_bar(stat="identity", fill="tan1") +
+      #expand_limits(y=c(0,35000))+
+      theme_economist()+
+      labs(title = paste("Male vs Females age", input$age), x = "Sex", y = "Number of accidents") + # accident type changes
+      theme(plot.title = element_text(hjust = 0.5))
+    print(age_bargraph)
+  })
+  output$demoGraph <- renderPlot({
+    demo_graph()
   })
   output$victimTypeTable <- renderTable({
     victim_types %>% dplyr::rename("Passenger Cars"=colnames(victim_types)[2],"Light Trucks"=colnames(victim_types)[3],
